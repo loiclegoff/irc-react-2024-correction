65087b75cb1682e6232cf11dcfb24ca693aa7f04
Loïc Le Goff <dev.legoff@gmail.com>
2021-10-14 12:50:51 +0200

    ✨ feat(app): first component with initial title and remove useless files created by create-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+  // set the initial title value with injected props
+  const [title, setTitle] = useState(props.title);
+
+  //function with the event as parameter
+  const handleChangeTitle = (e) => setTitle(e.target.value);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>this is my first React Component </h1>
+      <label htmlFor="titleInput">Title</label>
+      <input
+        type="text"
+        className="form-control"
+        id="titleInput"
+        onChange={handleChangeTitle}
+        value={title}
+      />
+      <h3>{title}</h3>
+    </div>
+  );
 }
 
-export default App
+export default App;
