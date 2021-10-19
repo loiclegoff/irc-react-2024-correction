cdae67b0c7c246f378438befccd63c065de080a6
Loïc Le Goff <dev.legoff@gmail.com>
2021-10-19 08:52:11 +0200

    ♻️ refactor(robot): move fetching in RobotList

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { PartList } from './components/PartList';
 import { RobotList } from './components/RobotList';
 
-const getPartIdsFromRobotId = (robotList, id) => {
-  const currentRobot = robotList.find((robot) => robot.id === id);
-  return currentRobot?.parts ?? [];
-};
-
 function App() {
-  const [robotList, setRobotList] = useState([]);
-  const [selectedRobotId, setSelectedRobotId] = useState(undefined);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const resp = await fetch(
-        'https://robot-cpe-2024.cleverapps.io/robots'
-      );
-      const robotList = await resp.json();
-      setRobotList(robotList);
-    };
-    fetchData();
-  }, []);
-
-  const handleOnRobotSelected = (id) => {
-    setSelectedRobotId(id);
-  };
-
-  const selectedPartIds = getPartIdsFromRobotId(
-    robotList,
-    selectedRobotId
-  );
+  const [selectedPartIds, setSelectedPartIds] = useState([]);
 
   return (
     <Container fluid>
@@ -40,9 +14,9 @@ function App() {
       <Row>
         <Col md={4} lg={4}>
           <RobotList
-            robotList={robotList}
-            selectedRobotId={selectedRobotId}
-            onRobotSelected={handleOnRobotSelected}
+            onRobotSelected={(robot) =>
+              setSelectedPartIds(robot.parts)
+            }
           />
         </Col>
         <PartList selectedPartIds={selectedPartIds} />
--- src/components/RobotList.jsx
+++ src/components/RobotList.jsx
@@ -1,12 +1,41 @@
-import { Spinner } from 'react-bootstrap'
-import { Robot } from './robot/Robot'
+import { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { Robot } from './robot/Robot';
 
 export function RobotList(props) {
-  if (props.robotList.length < 1) {
-    return <Spinner animation='border' />
+  const [robotList, setRobotList] = useState([]);
+  const [selectedRobotId, setSelectedRobotId] = useState(undefined);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetch(
+        'https://robot-cpe-2024.cleverapps.io/robots'
+      );
+      const robotList = await resp.json();
+      setRobotList(robotList);
+    };
+    fetchData();
+  }, []);
+
+  const onRobotSelected = (robot) => {
+    props.onRobotSelected(robot);
+    setSelectedRobotId(robot.id);
+  };
+
+  if (robotList.length < 1) {
+    return <Spinner animation="border" />;
   }
 
-  return <div>
-    {props.robotList.map(robot => <Robot key={robot.id} robot={robot} selectedRobotId={props.selectedRobotId} onRobotSelected={props.onRobotSelected} />)}
-  </div>
+  return (
+    <div>
+      {robotList.map((robot) => (
+        <Robot
+          key={robot.id}
+          robot={robot}
+          selectedRobotId={selectedRobotId}
+          onRobotSelected={onRobotSelected}
+        />
+      ))}
+    </div>
+  );
 }
