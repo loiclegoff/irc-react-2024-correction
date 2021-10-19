cdce865f610a6698f9b08290e6a7316990c2ae50
Loïc Le Goff <dev.legoff@gmail.com>
2021-10-19 09:23:06 +0200

    ♻️ refactor(RobotList): link to the robot reducer

--- src/components/RobotList.jsx
+++ src/components/RobotList.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Spinner } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { setRobots } from '../core/actions';
+import { selectRobots } from '../core/selectors';
 import { Robot } from './robot/Robot';
 
 export function RobotList(props) {
-  const [robotList, setRobotList] = useState([]);
   const [selectedRobotId, setSelectedRobotId] = useState(undefined);
+  const dispatch = useDispatch();
+  const robotList = useSelector(selectRobots);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,10 +17,10 @@ export function RobotList(props) {
         'https://robot-cpe-2024.cleverapps.io/robots'
       );
       const robotList = await resp.json();
-      setRobotList(robotList);
+      dispatch(setRobots(robotList));
     };
     fetchData();
-  }, []);
+  }, [dispatch]);
 
   const onRobotSelected = (robot) => {
     props.onRobotSelected(robot);
