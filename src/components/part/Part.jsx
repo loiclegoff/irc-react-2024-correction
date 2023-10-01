08ae32bcfb423c1026d06e0354d30550b29df85f
Loïc <dev.legoff@gmail.com>
2023-10-01 19:59:12 +0200

    ♻️ refactor: move getting of the selected part for description in reducer

--- src/components/part/Part.jsx
+++ src/components/part/Part.jsx
@@ -1,11 +1,22 @@
-import { Card } from 'react-bootstrap'
-import { Description } from './Description'
+import { Card } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { setSelectedPart } from '../../core/actions';
+import { selectSelectedPartId } from '../../core/selectors';
+import { Description } from './Description';
 
 export function Part(props) {
+  const dispatch = useDispatch();
+  const selectedPartId = useSelector(selectSelectedPartId);
+
+  const onPartSelected = () => {
+    dispatch(setSelectedPart(props.part.id));
+  };
   return (
-    <Card className='mb-2'
-      onClick={props.onPartClicked}
-      bg={props.part.id === props.selectedPartId ? "primary" : "default"}
+    <Card
+      className="mb-2"
+      onClick={onPartSelected}
+      bg={props.part.id === selectedPartId ? 'primary' : 'default'}
     >
       <Card.Header>
         <Card.Title>Part {props.part.id} description</Card.Title>
@@ -14,5 +25,5 @@ export function Part(props) {
         <Description part={props.part} />
       </Card.Body>
     </Card>
-  )
+  );
 }
